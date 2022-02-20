c9401a8bfe318f4c24131718c795518a61a8a8ff
w0rp <devw0rp@gmail.com>
2022-02-20 13:59:53 +0000

    Remove debug information from builds

--- build.sh
+++ build.sh
@@ -6,6 +6,6 @@ for executable in migrate ingest notify adduser pricewarp; do
     (
         echo "Building bin/$executable..."
         cd "cmd/$executable"
-        go build -o "../../bin/$executable"
+        go build -ldflags "-s -w" -o "../../bin/$executable"
     )
 done
